489f729275361f25a18c0a08437414fcb618a93c
Samuel Laforme <laforme.s@hotmail.com>
2022-03-24 12:06:41 -0400

    tutorial is done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # RESTapi with Typescript
 
 Hello, this is me trying to better understand typescript and creating a REST api while having some fun!
+
+For this learning, I'm following a small tutorial you can find [here](https://www.toptal.com/express-js/nodejs-typescript-rest-api-pt-1)
--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -5,4 +5,37 @@ export class UsersRoutes extends CommonRoutesConfig{
     constructor(app: express.Application) {
         super(app, 'UsersRoutes');
     }
+
+    configureRoutes(): express.Application {
+        // We'll add the actual route configuration here
+        this.app.route('/users')
+            .get((req: express.Request, res: express.Response) => {
+                res.status(200).send('List of users')
+            })
+            .post((req: express.Request, res: express.Response) => {
+                res.status(200).send('Post to users')
+            });
+        
+        this.app.route('/users/:userId')
+            .all((req: express.Request, res: express.Response, next: express.NextFunction) => {
+                // This middleware function runs before any request to /users/:userId
+                // but it doesn't accomplish anything just yet---
+                // it simply passes control to the next applicable function below using next()
+                next();
+            })
+            .get((req: express.Request, res: express.Response) => {
+                res.status(200).send(`GET requested for id ${req.params.userId}`)
+            })
+            .put((req: express.Request, res: express.Response) => {
+                res.status(200).send(`PUT requested for id ${req.params.userId}`)
+            })
+            .patch((req: express.Request, res: express.Response) => {
+                res.status(200).send(`patch requested for id ${req.params.userId}`)
+            })
+            .delete((req: express.Request, res: express.Response) => {
+                res.status(200).send(`delete requested for id ${req.params.userId}`)
+            })
+            
+        return this.app;
+    }
 }
